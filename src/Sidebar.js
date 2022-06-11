80d53e67c7ce23c2dea1c277b35097167450f88e
Sahil Bose <bosesahil44@gmail.com>
2022-06-11 11:09:25 +0530

    Modified: Sidebar and SidebarOptions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,9 @@
-import { Button } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import React from 'react';
 import './Sidebar.css';
 import SidebarOptions from './SidebarOptions';
 import AddIcon from "@mui/icons-material/Add"
-import { Inbox } from '@mui/icons-material';
+import { AccessTime, Duo, ExpandMore, Inbox, LabelImportant, NearMe, Note, Person, Phone, Star} from '@mui/icons-material';
 
 function Sidebar() {
   return (
@@ -15,7 +15,27 @@ function Sidebar() {
       Compose
         </Button>
 
-      <SidebarOptions Icon={Inbox} title="Inbox" number={54}/>
+        <SidebarOptions Icon={Inbox} title="Inbox" number={54} selected ={true}/>
+        <SidebarOptions Icon={Star} title="Starred" number={54}/>
+        <SidebarOptions Icon={AccessTime} title="Snoozed" number={54}/>
+        <SidebarOptions Icon={LabelImportant} title="Important" number={54}/>
+        <SidebarOptions Icon={NearMe} title="Sent" number={54}/>
+        <SidebarOptions Icon={Note} title="Drafts" number={54}/>
+        <SidebarOptions Icon={ExpandMore} title="More" number={54}/>
+
+        <div className="sidebar__footer">
+          <div className="sidebar__footerIcons">
+              <IconButton>
+                <Person/>
+              </IconButton>
+              <IconButton>
+                <Duo/>
+              </IconButton>
+              <IconButton>
+                <Phone/>
+              </IconButton>
+          </div>
+        </div>
     </div>
   )
 }
--- src/SidebarOptions.js
+++ src/SidebarOptions.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './SidebarOptions.css';
 
-function SidebarOptions({Icon, title, number}) {
+function SidebarOptions({Icon, title, number, selected}) {
   return (
-    <div className="sidebarOptions">
+    <div className={`sidebarOptions ${selected && 'sidebarOptions--active'}`}>
         <Icon />
         <h3>{title}</h3>
         <p>{number}</p>
     </div>
-  )
+  );
 }
 
 export default SidebarOptions
