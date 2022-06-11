8a0f75ee9792722824c27235c99b7b4b48b1c619
Sahil Bose <bosesahil44@gmail.com>
2022-06-11 11:28:29 +0530

    Build:Email and Email section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-
+import {BrowserRouter as Router, Switch , Route , Link} from "react-router-dom" ;
 
 function App() {
   return (
+      <Router>
     <div className="app">
       <Header/>
       <Sidebar/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/EmailList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './EmailList.css'
+function EmailList() {
+  return (
+    <div>EmailList</div>
+  )
+}
+
+export default EmailList
