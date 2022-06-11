f438119e062d66082b4b1d0d3cfc2c3b5f515b10
Sahil Bose <bosesahil44@gmail.com>
2022-06-11 10:02:45 +0530

    Modified:Sidebar , Added: SidebarOptions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React from 'react';
 import './Sidebar.css';
+import SidebarOptions from './SidebarOptions';
+import AddIcon from "@mui/icons-material/Add"
+import { Inbox } from '@mui/icons-material';
 
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <Button 
+        startIcon = {<AddIcon fontSize = "large"/>}
+        className ="sidebar__compose"
+      > 
+      Compose
+        </Button>
+
+      <SidebarOptions Icon={Inbox} title="Inbox" number={54}/>
+    </div>
   )
 }
 
