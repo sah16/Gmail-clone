9a9d47c7172bd53985089dcd33f7af390239f599
Sahil Bose <bosesahil44@gmail.com>
2022-06-14 15:58:56 +0530

    Added: Login and Logout  functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import {BrowserRouter as Router, Routes , Route , Link} from "react-router-dom" ;
+import {BrowserRouter as Router, Routes , Route } from "react-router-dom" ;
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
 import { useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { login, selectUser } from './features/userSlice';
+import Login from './Login';
+import { auth } from './firebase';
+import {useDispatch} from 'react-redux';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch =useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(user => {
+      if (user){
+      //the user is logged in
+          dispatch(login({
+            displayName: user.displayName,
+            email:user.email,
+            photoUrl:user.photoURL,
+          }))
+    }  
+    })
+  },[])
 
   return (
       <Router>
+
+        {!user ? (
+          <Login />
+        ): (
         <div className="app">
           <Header/>
 
@@ -29,6 +52,7 @@ function App() {
             </div>
           </div>
         </div>
+        )}
       </Router>
   );
 }
